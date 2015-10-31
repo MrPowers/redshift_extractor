7431d67c9dccafc73a297f50f8cc117b739cb283
MrPowers <matthewkevinpowers@gmail.com>
2015-10-31 16:24:32 -0500

    Finish up the extractor specs

--- spec/redshift_extractor/extractor_spec.rb
+++ spec/redshift_extractor/extractor_spec.rb
@@ -51,5 +51,70 @@ module RedshiftExtractor; describe Extractor do
     end
   end
 
+  context "#dropper" do
+    it "instantiates a Drop object" do
+      args = {table_name: "table_name"}
+      expect(Drop).to receive(:new).with(args)
+      extractor.send(:dropper)
+    end
+  end
+
+  context "#drop" do
+    it "runs the drop_sql in the database" do
+      destination_connection = double
+      expect(extractor).to receive(:destination_connection).and_return(destination_connection)
+      expected_sql = "drop table if exists table_name;"
+      expect(destination_connection).to receive(:exec).with expected_sql
+      extractor.send(:drop)
+    end
+  end
+
+  context "#create" do
+    it "creates a database table" do
+      destination_connection = double
+      expect(extractor).to receive(:destination_connection).and_return(destination_connection)
+      expected_sql = "create_sql"
+      expect(destination_connection).to receive(:exec).with expected_sql
+      extractor.send(:create)
+    end
+  end
+
+  context "#copier" do
+    it "instantiates an Copy object" do
+      args = {
+        data_source: "copy_data_source",
+        table_name: "table_name",
+        aws_access_key_id: "aws_access_key_id",
+        aws_secret_access_key: "aws_secret_access_key"
+      }
+      expect(Copy).to receive(:new).with(args)
+      extractor.send(:copier)
+    end
+  end
+
+  context "#copy" do
+    it "runs the copy sql command" do
+      destination_connection = double
+      expect(extractor).to receive(:destination_connection).and_return(destination_connection)
+      expected_sql = "copy table_name from 'copy_data_source' credentials 'aws_access_key_id=aws_access_key_id;aws_secret_access_key=aws_secret_access_key' manifest dateformat 'auto' timeformat 'auto' blanksasnull emptyasnull escape gzip removequotes delimiter '|';"
+      expect(destination_connection).to receive(:exec).with expected_sql
+      extractor.send(:copy)
+    end
+  end
+
+  context "#destination_connection" do
+    it "establishes a connection with the destination database" do
+      expect(PGconn).to receive(:connect).with("database_config_destination")
+      extractor.send(:destination_connection)
+    end
+  end
+
+  context "#source_connection" do
+    it "establishes a connection with the source database" do
+      expect(PGconn).to receive(:connect).with("database_config_source")
+      extractor.send(:source_connection)
+    end
+  end
+
 end; end
 
